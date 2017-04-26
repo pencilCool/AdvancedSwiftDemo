8aec6e8ffacdec890c1e51607833cc27b05a6819
pencilCool <yhtang_xt@163.com>
2017-04-26 10:08:54 +0800

    sequence done

--- AdvancedSwift.playground/Pages/Sequences.xcplaygroundpage/Contents.swift
+++ AdvancedSwift.playground/Pages/Sequences.xcplaygroundpage/Contents.swift
@@ -222,13 +222,143 @@ let randomNumbers = sequence(first: 100) { (previous: UInt32) in
 Array(randomNumbers) // [100, 83, 27, 13, 10, 2, 1]
 
 //  还是写一个求最大公约数
-func nextTuple(_ tuple :(Int,Int)) -> (Int,Int)? {
-        let bigNum    = max(tuple.0, tuple.1)
-        let smallNum  = max(tuple.0, tuple.1)
-        let newValue  = (bigNum-smallNum, smallNum )
-        return newValue;
+
+let commonDivisorFunction = sequence(first: (20,30)) {
+    tuple  in
+    print(tuple)
+    guard tuple.0 == 0 else {
+        return nil
+    }
+    let bigNum    = max(tuple.0, tuple.1)
+    print("bigNum = \(bigNum)")
+    let smallNum  = min(tuple.0, tuple.1)
+    print("smallNum = \(smallNum)")
+    let newValue  = (bigNum-smallNum, smallNum )
+    print("newValue = \(newValue)")
+  
+    return newValue;  // 这个结果
+    
 }
-let commonDivisorFunction = sequence(first: (5,8), next:nextTuple)
 Array(commonDivisorFunction)
 
+// 另一个强大的序列发生器
+
+let  fibsSequence2 = sequence(state: (0, 1)) {
+    // The compiler needs a little type inference help here 
+    (state: inout (Int, Int)) -> Int? in
+    let upcomingNumber = state.0
+    state = (state.1, state.0 + state.1)  // 这个state的状态，不用return 也能保存给下一个迭代用
+    return upcomingNumber
+}
+
+Array(fibsSequence2.prefix(10)) // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+// 还是求最大公约数
+
+let cmdsequence = sequence(state: (60,24)){
+    (state: inout (Int, Int)) -> Int? in
+    
+    let bigNum     = max(state.0,state.1)
+    let smallNum   = min(state.0,state.1)
+    state          = (smallNum,bigNum - smallNum)
+    let result     = bigNum
+    if smallNum == 0 {
+        return nil
+    }
+    return result;
+}
+
+Array(cmdsequence.prefix(12))
+
+// 序列发生器 是 lazy 的，别人不问他序列是什么样的，他就不会计算。
+// 如果一开始就把计算算了，那么程序可能会因为 溢出 而crash 的
+// sequence 能创造 无线的序列 ，而其他的collection 类型却不能。
+
+
+//MARK: 不稳定的序列
+// 序列不仅包括类容器，数据结构，比如array or list
+// 他还包括 网络数据流， 磁盘文件，UI的event 消息流，所有的的写数据都可以归类为sequence
+// 但是这些和array 是不一样的，你的迭代器可以访问一个元素多次
+
+
+// 像斐波拉契序列，你遍历他的时候，他的数据不会被你消耗，每一次遍历的结果都一样
+// 但是像网络流，你遍历序列的时候，网络流数据就被你消耗了。每一次遍历的结果都不一样
+// 这两种都是合法的序列，所以 文档中支持 序列不能保证多次遍历结果的唯一性。
+
+// 这也就解释了为什么 只有colletion 含有 获取first 等方法，而sequence 中没有定义这样的
+// 因为 colleciton 能保证get 函数 没有副作用，不会销毁访问的数据
+
+
+// 考虑一种带有销毁功能的序列：
+let standardIn = AnySequence {
+    return AnyIterator {
+        readLine()
+    }
+}
+// 这样就可以用上给sequence的扩展了。
+
+let numberedStdIn = standardIn.enumerated()
+for (i, line) in numberedStdIn {
+    print("\(i+1): \(line)")
+}
+
+// 实验结果：
+//1:
+//
+//2:
+//fdsa
+//3: fdsa
+//dadas;
+//4: dadas;
+//ff
+//5: ff
+
+// sequence 的实现这不需要总是记住 遍历的副作用(销毁数据) ，但是作为sequence 的调用者，这点必须牢记
+
+// sequence对象 没有副作用的一个信号是：这个对象还实现了 collection 的协议。但是稳定不一定就一定实现了collection协议
+// StrideTo  就可以遍历多次，但是它没事实现collection 协议。
+
+// MARK: 序列和迭代器的关系
+// 看起来很相似，为什么不把 IteratorProtocol 放到 sequence 里面去呢？
+// 其实有些 sequence 是自己带了一个迭代器，在遍历的时候一直改变状态。
+// 每一个迭代器在他返回数据之前，可以看做是一个不稳定的sequence 。
+// 其实你可以将所有的迭代器 转变成 序列。 只要你实现了协议。  (Doubt)
+
+
+// MARK: Subsequences
+
+//
+//protocol Sequence {
+//    associatedtype Iterator: IteratorProtocol
+//    associatedtype SubSequence
+//    // ...
+//}
+// 是用来返回slice 和其他元 序列的
+// 比如 prefix 和 suffix ， dropFirst 和 dropFirst split
+// 如果你不设置特定的 subsequence ，那么编译器的默认实现就是AnySequence<Iterator.Element> 
+// 因为sequence 提供了上面个这些函数的默认实现。
+// 通常如果 subsequence == self 是很方便的实现。比如 string.CharacterView 
+
+
+// 讨论了一种理想的情况。(Doubt page 62)
+
+//associatedtype SubSequence: Sequence
+//where Iterator.Element == SubSequence.Iterator.Element,
+//SubSequence.SubSequence == SubSequence
+
+//1 满足 subsequence 也是一个序列
+//2 满足 subsequence 的元素和sequence 的元素是一直的。而他们的subsequence都是一样的。
+// 但是这样的定义方式 编译器是不能接受的。   就是 协议递归的定义不支持。
+
+extension Sequence
+    where Iterator.Element: Equatable,
+    SubSequence: Sequence,
+SubSequence.Iterator.Element == Iterator.Element {
+    func headMirrorsTail(_ n: Int) -> Bool { let head = prefix(n)
+        let tail = suffix(n).reversed()
+        return head.elementsEqual(tail)  // elmentsEqual 要求 head 和tail (也就是 subsequence) 也必须是sequence，并且类型也要是和sequnce 的元素类型是相同的，要不然就不能用 Equatable 了
+    } }
+[1,2,3,4,2,1].headMirrorsTail(2) // true 检查首尾相同数字的个数是不是达到要求
+
+//
 
